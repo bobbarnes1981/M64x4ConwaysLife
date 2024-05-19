db51d42454cc1008107c3f9da8bd80556ffc2377
Robert Barnes <robertbarnes1981@googlemail.com>
2024-05-19 06:36:15 +0100

    Update conway.asm

--- conway.asm
+++ conway.asm
@@ -1,6 +1,7 @@
 ; TODO
 ;       > create tool to show keypress hex code
 ;       > display title 0,0
+;       > grid 40x24
 ;       > draw cursor box on 10 pixel grid
 ;       > move cursor box on 10 pixel grid using arrow keys
 ;       > use space bar to enable/disable cell 8x8
@@ -10,5 +11,9 @@
 ;       > count steps taken
 ;       > display steps taken 0,29
 ;       > reserve memory for two representations if screen
+;       > add flag for which cell pointer is to be used
 ;       > loop through all cells in screen (increment cell pointer and alt cell pointer)
-;       > 
+;       > check neighbours using offsets in cell pointer
+;       > update cell state in alternative cell pointer
+;       > update cell on screen
+;       > update flag to correct cell pointer
