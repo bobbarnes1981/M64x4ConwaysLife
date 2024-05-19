7d93f533b8e5517ad5dc712fcbcbf4936c418457
Robert Barnes <robertbarnes1981@googlemail.com>
2024-05-19 06:01:44 +0100

    Create conway.asm

---
+++ conway.asm
@@ -0,0 +1,14 @@
+; TODO
+;       > create tool to show keypress hex code
+;       > display title 0,0
+;       > draw cursor box on 10 pixel grid
+;       > move cursor box on 10 pixel grid using arrow keys
+;       > use space bar to enable/disable cell 8x8
+;       > use enter to start simulation
+;       > use 'p' to pause/unpause simulation
+;       > use 'esc' to exit simulation
+;       > count steps taken
+;       > display steps taken 0,29
+;       > reserve memory for two representations if screen
+;       > loop through all cells in screen (increment cell pointer and alt cell pointer)
+;       > 
